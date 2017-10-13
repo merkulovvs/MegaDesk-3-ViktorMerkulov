218fd1b0178832003fe445eb9b1e173065d80f31
merkulovvs <mervic81@gmail.com>
2017-10-13 18:38:53 +0300

    Added required classes

---
+++ MegaDesk-3-ViktorMerkulov/Desk.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace MegaDesk_3_ViktorMerkulov
+{
+    class Desk
+    {
+        public int width { get; set; }
+        public int depth { get; set; }
+        public int numDrawers { get; set; }
+        public string material { get; set; }
+    }
+}
---
+++ MegaDesk-3-ViktorMerkulov/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaDesk_3_ViktorMerkulov
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
